389f8337b41cf3a6f78a33aa862b8a918ee559d5
pegas <tptb123666@gmail.com>
2024-09-15 12:10:24 +0530

    asdasd

---
+++ src/main/java/com/hackathon/thinq/ThinqApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.thinq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/thinq/ThinqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.thinq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
